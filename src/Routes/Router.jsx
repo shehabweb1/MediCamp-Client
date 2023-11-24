84ef720f6da35518ec3ab0ff946ea2ce0be77c31
MD Shehab Hossen <shehab.web1@gmail.com>
2023-11-25 00:42:30 +0600

    Layout Done

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Main />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+		],
+	},
+]);
+
+export default router;
