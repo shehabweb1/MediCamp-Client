8f6d0bd1d5911e1e105e913c490895941896f89b
MD Shehab Hossen <shehab.web1@gmail.com>
2023-12-05 22:18:20 +0600

    Done

--- src/Components/Healthcare.jsx
+++ src/Components/Healthcare.jsx
@@ -18,12 +18,21 @@ const Healthcare = () => {
 	return (
 		<div>
 			<PageHeader title="Our Healthcare Professional" />
-			<div className="grid">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 pb-5">
 				{healthcareData?.map((healthcare) => (
-					<div key={healthcare._id}>
-						<img src={healthcare.image} alt={healthcare.name} />
-						<h3>{healthcare.name}</h3>
-						<p>{healthcare.specialist}</p>
+					<div
+						key={healthcare._id}
+						className="rounded-xl shadow-xl bg-[#054279] text-white"
+					>
+						<img
+							src={healthcare.image}
+							alt={healthcare.name}
+							className="w-full h-auto rounded-t-xl"
+						/>
+						<h3 className="text-2xl font-bold text-center mt-3">
+							{healthcare.name}
+						</h3>
+						<p className="mb-3 text-center">{healthcare.specialist}</p>
 					</div>
 				))}
 			</div>
